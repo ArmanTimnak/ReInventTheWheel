2c6968fac9d88756527d6cc626f1ec6b091398af
ArmanTimnak <timnak.manager@gmail.com>
2023-12-06 21:08:10 +0330

    Add common_factors function

---
+++ math/common_factors.py
@@ -0,0 +1,24 @@
+def common_factors(*numbers):
+    """
+    Returns a set of common factors of the given numbers.
+    
+    Parameters:
+    numbers (int): The numbers to find common factors for.
+    
+    Returns:
+    set: A set of common factors.
+
+    Example:
+        >>> common_factors(12, 15)
+        {3}
+        >>> common_factors(12, 15, 20)
+        {4, 5}
+    """
+    factors = set()
+    smallest_number = min(numbers)
+    
+    for i in range(1, smallest_number + 1):
+        if all(num % i == 0 for num in numbers):
+            factors.add(i)
+    
+    return factors
