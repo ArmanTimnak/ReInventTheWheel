66ee04fa19fc38edfc6cee3ab0161bd307926d48
ArmanTimnak <timnak.manager@gmail.com>
2023-12-06 19:37:58 +0330

    Added docs

--- math/nth_prime.py
+++ math/nth_prime.py
@@ -1,4 +1,19 @@
 def is_prime(n):
+    """
+    Check if a number is prime.
+
+    Args:
+        n (int): The number to check.
+
+    Returns:
+        bool: True if the number is prime, False otherwise.
+
+    Example:
+        >>> is_prime(2)
+        True
+        >>> is_prime(4)
+        False
+    """
     if n < 2:
         return False
     for i in range(2, int(n**0.5) + 1):
@@ -7,6 +22,23 @@ def is_prime(n):
     return True
 
 def nth_prime(n):
+    """
+    Returns the nth prime number.
+
+    Parameters:
+    n (int): The position of the prime number to be found.
+
+    Returns:
+    int: The nth prime number.
+
+    Example:
+        >>> nth_prime(1)
+        2
+        >>> nth_prime(2)
+        3
+        >>> nth_prime(10)
+        29
+    """
     count = 0
     num = 2
     while count < n:
