8fd24679dba8527e27c86b3538e6cd7a5b834c84
ArmanTimnak <timnak.manager@gmail.com>
2023-12-17 20:20:41 +0330

    Add decimal_to_binary function with docstring

--- convert/decimal_to_binary.py
+++ convert/decimal_to_binary.py
@@ -1,4 +1,21 @@
 def decimal_to_binary(decimal):
+    """
+    Converts a decimal number to its binary representation.
+
+    Args:
+        decimal (int): The decimal number to be converted.
+
+    Returns:
+        str: The binary representation of the decimal number.
+
+    Example:
+        >>> decimal_to_binary(10)
+        '1010'
+        >>> decimal_to_binary(0)
+        '0'
+        >>> decimal_to_binary(1)
+        '1'
+    """
     if decimal == 0:
         return '0'
     
